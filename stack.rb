42ffa34a3df645e61031902a4704776d87338cb4
Katrina <blueyed_qt@hotmail.com>
2015-11-08 09:23:17 -0800

    Set up for Stack Fun

---
+++ stack.rb
@@ -0,0 +1,4 @@
+class Stack
+	# additionally, you will need to create a 'stack_display' function that will simply return the @data_store variable for the tests to pass, because one of the test cases utilizes this function to validate the @data_store array.
+
+end
